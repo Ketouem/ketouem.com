6004ca23a03a385517f832d1aa4574e8a2f876e4
Ketouem <ketouem@gmail.com>
2020-09-04 16:03:13 +0200

    [ci skip] Update ACM cert & bump tf version to 0.13.2

--- terraform/common.tf
+++ terraform/common.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 0.10.8"
+  required_version = "~> 0.13.2"
 }
 
 provider "aws" {
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,14 @@
 data "aws_acm_certificate" "cert" {
-  provider = "aws.us-east-1"
-  domain   = "${var.domain_name}"
-  statuses = ["ISSUED"]
+  provider    = aws.us-east-1
+  domain      = var.domain_name
+  statuses    = ["ISSUED"]
+  types       = ["AMAZON_ISSUED"]
+  most_recent = true
 }
 
 # S3
 resource "aws_s3_bucket" "ketouem_com" {
-  bucket        = "${var.domain_name}"
+  bucket        = var.domain_name
   acl           = "public-read"
   force_destroy = false
 
@@ -19,13 +21,13 @@ resource "aws_s3_bucket" "ketouem_com" {
 # Cloudfront
 resource "aws_cloudfront_distribution" "ketouem_com" {
   enabled             = true
-  aliases             = ["${var.domain_name}"]
+  aliases             = [var.domain_name]
   default_root_object = "index.html"
   is_ipv6_enabled     = true
   price_class         = "PriceClass_100"
 
   origin {
-    domain_name = "${aws_s3_bucket.ketouem_com.bucket_domain_name}"
+    domain_name = aws_s3_bucket.ketouem_com.bucket_domain_name
     origin_id   = "S3-${var.domain_name}"
   }
 
@@ -56,7 +58,7 @@ resource "aws_cloudfront_distribution" "ketouem_com" {
 
   viewer_certificate {
     # Warning: certificate must be in us-east-1
-    acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
+    acm_certificate_arn = data.aws_acm_certificate.cert.arn
     ssl_support_method  = "sni-only"
   }
 }
@@ -67,13 +69,13 @@ resource "aws_route53_zone" "primary" {
 }
 
 resource "aws_route53_record" "site_record" {
-  zone_id = "${aws_route53_zone.primary.zone_id}"
+  zone_id = aws_route53_zone.primary.zone_id
   name    = "${var.domain_name}."
   type    = "A"
 
   alias {
-    name                   = "${aws_cloudfront_distribution.ketouem_com.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.ketouem_com.hosted_zone_id}"
+    name                   = aws_cloudfront_distribution.ketouem_com.domain_name
+    zone_id                = aws_cloudfront_distribution.ketouem_com.hosted_zone_id
     evaluate_target_health = false
   }
 }
